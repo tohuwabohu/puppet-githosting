3455d516ca50d9103d2de32f27ba4ce6e76ca0f6
Martin Meinhold <Martin.Meinhold@gmx.de>
2013-11-30 14:01:50 +1100

    Update documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: githosting
 #
-# Full description of class githosting here.
+# Manage git repositories accessible from outside to a group of users.
 #
 # === Parameters
 #
@@ -10,30 +10,13 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { githosting:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
-# Author Name <author@domain.com>
+# Martin Meinhold <Martin.Meinhold@gmx.de>
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Martin Meinhold
 #
 class githosting {
 
