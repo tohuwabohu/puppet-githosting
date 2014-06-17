a7afc7f56493fea8623290d2721e5f82bf11344e
Martin Meinhold <Martin.Meinhold@gmx.de>
2014-06-17 20:48:15 +1000

    Review test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,60 +1,60 @@
 require 'spec_helper'
 
 describe 'githosting' do
-    let(:title) { 'githosting' }
+  let(:title) { 'githosting' }
 
   describe 'installs custom git package' do
     let(:params) { {:git_package => 'git2'} }
 
-    it { should contain_package('git2') }
+    specify { should contain_package('git2') }
   end
 
   describe 'installs latest git by default' do
     let(:params) { {} }
 
-    it { should contain_package('git').with_ensure('latest') }
+    specify { should contain_package('git').with_ensure('latest') }
   end
 
   describe 'installs custom git version' do
     let(:params) { {:git_version => '1.2.3'} }
 
-    it { should contain_package('git').with_ensure('1.2.3') }
+    specify { should contain_package('git').with_ensure('1.2.3') }
   end
 
   describe 'uses custom git executable' do
     let(:params) { {:git_executable => '/somewhere/else/git', :repositories => ['foobar']} }
 
-    it { should contain_githosting__repository('foobar').with_git_executable('/somewhere/else/git') }
+    specify { should contain_githosting__repository('foobar').with_git_executable('/somewhere/else/git') }
   end
 
   describe 'creates user git by default' do
     let(:params) { {} }
 
-    it { should contain_user('git').with_ensure('present') }
+    specify { should contain_user('git').with_ensure('present') }
   end
 
   describe 'sets git shell' do
     let(:params) { {} }
 
-    it { should contain_user('git').with_shell('/usr/bin/git-shell') }
+    specify { should contain_user('git').with_shell('/usr/bin/git-shell') }
   end
 
   describe 'creates custom user' do
     let(:params) { {:service => 'foobar'} }
 
-    it { should contain_user('foobar').with_ensure('present') }
+    specify { should contain_user('foobar').with_ensure('present') }
   end
 
   describe 'creates home directory by default' do
     let(:params) { {} }
 
-    it { should contain_user('git').with_home('/var/git') }
+    specify { should contain_user('git').with_home('/var/git') }
   end
 
   describe 'creates custom home directory' do
     let(:params) { {:data_dir => '/somewhere/else'} }
 
-    it { should contain_user('git').with_home('/somewhere/else') }
+    specify { should contain_user('git').with_home('/somewhere/else') }
   end
 
   describe 'grant user access' do
@@ -65,12 +65,12 @@ describe 'githosting' do
       user   => 'foobar',
     }" }
 
-    it { should contain_githosting__authorized_user('foobar') }
+    specify { should contain_githosting__authorized_user('foobar') }
   end
 
   describe 'creates git repository' do
     let(:params) { {:repositories => ['foobar']} }
 
-    it { should contain_githosting__repository('foobar').with_ensure('present') }
+    specify { should contain_githosting__repository('foobar').with_ensure('present') }
   end
 end
